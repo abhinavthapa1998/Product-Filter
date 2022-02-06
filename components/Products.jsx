ef013f51dfb16d634475accc7a367535ab08b42e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-02-06 22:41:34 +0530

    commit

--- components/Products.jsx
+++ components/Products.jsx
@@ -5,47 +5,35 @@ import Product from "./Product";
 import Testing from "./Testing";
 
 const Products = () => {
-  
   const url = "https://assessment-edvora.herokuapp.com/";
   const [loading, setLoading] = useState(true);
   const [prods, setProd] = useState({});
   useEffect(() => {
     fetchData();
   }, []);
-  
+
   const fetchData = () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setProd(data);
-        
+
         setLoading(false);
       });
   };
 
   const showData = () => {
-    let prodarray = Object.keys(prods); 
-    return(
-    <div className="product_array">
-      {prodarray.map((p,i) =>
-      {
-      return(
-      <Product key={i} index={i} productObject={prods}/>
-      )
-      }
-      )
-      }
-    
-    </div>)
-    }
-  
-  return (
-  <>
-  
-  {loading ? console.log("Data is loading") : showData()}
-  
-  </>
-  )
+    let prodarray = Object.keys(prods);
+    return (
+      <div className="product_array snaps-inline">
+        {prodarray.map((p, i) => {
+          return <Product key={i} index={i} productObject={prods} />
+        })}
+      </div>
+    );
+  };
+
+  return <>{loading ? console.log("Data is loading") : showData()}</>
 };
 
 export default Products;
