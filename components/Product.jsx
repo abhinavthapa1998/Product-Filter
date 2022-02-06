8158c414a07ec799c7e078852e5e3052a19da448
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-02-06 18:24:53 +0530

    commit

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,54 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
+  let i = parseInt(props.index);
+  let prods = props.prod;
+  let name = prods[i]["product_name"];
+  let brand = prods[i]["brand_name"];
+  let image = prods[i]["image"];
+  let price = prods[i]["price"];
+  let desc = prods[i]["discription"];
+  let location = prods[i]["address"]["state"];
+  let date = prods[i]["date"].substring(1, 10);
 
-let i = parseInt(props.index);
-
-const url = "https://assessment-edvora.herokuapp.com/";
-  const [loading, setLoading] = useState(true);
-  const [prods, setProd] = useState({});
-  useEffect(()=>{
-    fetchData();
-  },[])
-  const fetchData=()=>{
-  fetch(url)
-  .then(response =>response.json())
-  .then((data) =>{
-    setProd(data);
-    setLoading(false);
-  })
-}
-// let name = prods[i]["product_name"];
-// let brand = prods[i]["brand_name"];
-// let image = prods[i]["image"];
-// let price = prods[i]["price"]
-// let desc = prods[i]["discription"];
-// let location = prods[i]["address"]["state"];
-// let date = (prods[i]["date"]).substring(1,10);
-
-return (
-  <>
-  {loading?console.log("Data is loading"):
-  <div className="product_Container">
-    <div className="row1">
-    <div className="product_image">
-    <img src={prods[i]["image"]}/>
-    </div>
-    <div className="product_details">
-      <div className='name'>{prods[i]["product_name"]}</div>
-      <div className='brand'>{prods[i]["brand_name"]}</div>
-      <div className='price'>{prods[i]["price"]}</div>
-    </div>
-    </div>
-    <div className="row2">
-      <div className="loc">{prods[i]["address"]["state"]}</div>
-      <div className="date">{(prods[i]["date"]).substring(1,10)}</div>
-    </div>
-    <div className="description">{prods[i]["discription"]}</div>
-  </div>
-  }
-  </>
+  return (
+    <>
+      <div className="product_Container">
+        <div className="row1">
+          <div className="product_image">
+            <img src={image} />
+          </div>
+          <div className="product_details">
+            <div className="name">{name}</div>
+            <div className="brand">{brand}</div>
+            <div className="price">{price}</div>
+          </div>
+        </div>
+        <div className="row2">
+          <div className="loc">{location}</div>
+          <div className="date">{date}</div>
+        </div>
+        <div className="description">{desc}</div>
+      </div>
+    </>
   );
 };
 
--- components/Products.jsx
+++ components/Products.jsx
@@ -1,14 +1,39 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./Products.css";
 import Product from "./Product";
-import Testing from "./Testing";
 
 const Products = () => {
-  return (
+  
+  const url = "https://assessment-edvora.herokuapp.com/";
+  const [loading, setLoading] = useState(true);
+  const [prods, setProd] = useState({});
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setProd(data);
+        setLoading(false);
+      });
+  };
+
+  const showData = () => {
+    let productarr = Object.keys(prods); 
+    return(
     <div className="product_array">
-      <Product index={1} />
-    </div>
-  );
+      {productarr.map((pa, i) => {
+        <Product key={i} index={i} prod={prods} />
+      })}
+    </div>)
+  };
+  return (
+  <>
+  {loading ? console.log("Data is loading") : showData()}
+  </>
+  )
 };
 
 export default Products;
